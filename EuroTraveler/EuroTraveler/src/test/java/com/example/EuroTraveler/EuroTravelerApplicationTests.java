73ab99e214e88cd80e0fd2551de26ebef81bf85c
AcaJo <crazyaca45@gmail.com>
2022-05-22 01:32:26 +0200

    integracija

---
+++ EuroTraveler/EuroTraveler/src/test/java/com/example/EuroTraveler/EuroTravelerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EuroTraveler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuroTravelerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
