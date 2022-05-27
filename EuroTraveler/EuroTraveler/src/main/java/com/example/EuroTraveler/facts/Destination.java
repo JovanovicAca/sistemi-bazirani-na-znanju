9c0a33e35386ef1ad3430053a09f843934fa4ddd
Uros Blagojevic <71049196+blagojevic-uros@users.noreply.github.com>
2022-05-27 20:22:16 +0200

    Revert "Model"
    This reverts commit d010a43735d9ce0361dd02313a2a255986d7df6e.

---
+++ EuroTraveler/EuroTraveler/src/main/java/com/example/EuroTraveler/facts/Destination.java
@@ -0,0 +1,16 @@
+package com.example.EuroTraveler.facts;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Destination {
+    public String name;
+    public String description;
+
+}
